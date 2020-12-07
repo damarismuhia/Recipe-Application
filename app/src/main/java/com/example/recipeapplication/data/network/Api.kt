f26e48f160a177a2d0b49806069e3f0a818e4aa1
MosesWangira <40572173+MosesWangira@users.noreply.github.com>
2020-12-08 02:03:43 +0300

    define network object and Api interface

--- app/src/main/java/com/example/recipeapplication/data/network/Api.kt
+++ app/src/main/java/com/example/recipeapplication/data/network/Api.kt
@@ -1,4 +1,10 @@
 package com.example.recipeapplication.data.network
 
+import com.example.recipeapplication.data.models.Recipe
+import retrofit2.Call
+import retrofit2.http.GET
+
 interface Api {
+    @GET("api/search?q=chicken")
+    fun getAllRecipes(): Call<Recipe>
 }
--- app/src/main/java/com/example/recipeapplication/data/network/RetrofitBuilder.kt
+++ app/src/main/java/com/example/recipeapplication/data/network/RetrofitBuilder.kt
@@ -1,4 +1,21 @@
 package com.example.recipeapplication.data.network
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object RetrofitBuilder {
+    private const val BASE_URL: String = "https://recipesapi.herokuapp.com/"
+
+    private val retrofitBuilder: Retrofit.Builder by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+    }
+
+    val apiService: Api by lazy {
+        retrofitBuilder
+            .build()
+            .create(Api::class.java)
+    }
+
 }
