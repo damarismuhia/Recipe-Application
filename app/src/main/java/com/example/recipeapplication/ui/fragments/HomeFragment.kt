f9740de7723ad708e24e526def4b1241d51d29df
MosesWangira <40572173+MosesWangira@users.noreply.github.com>
2020-12-08 11:07:11 +0300

    load data to UI in gridlayout from API

--- app/src/main/java/com/example/recipeapplication/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/recipeapplication/ui/fragments/HomeFragment.kt
@@ -1,20 +1,60 @@
 package com.example.recipeapplication.ui.fragments
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.recipeapplication.R
+import com.example.recipeapplication.databinding.FragmentHomeBinding
+import com.example.recipeapplication.viewmodels.RecipesViewModel
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(R.layout.fragment_home) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    private lateinit var viewModel: RecipesViewModel
+    lateinit var binding: FragmentHomeBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = DataBindingUtil.bind(view)!!
+
+        viewModel = ViewModelProvider(this).get(RecipesViewModel::class.java)
+        binding.apply {
+            fragmentRecipeViewModel = viewModel
+            lifecycleOwner = this@HomeFragment
+        }
+        makeApiCallCoroutines()
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun makeApiCallCoroutines(): RecipesViewModel {
+        viewModel.getAllRecipesData().observe(viewLifecycleOwner, {
+            if (it != null) {
+                /**
+                 * update the adapter
+                 * */
+                binding.numberOfRecipes.text = "Number of chicken recipes : ${it.count}"
+
+                val gridLayoutManager =
+                    GridLayoutManager(requireContext(), 2, LinearLayoutManager.VERTICAL, false)
+                binding.recyclerView.apply {
+                    hasFixedSize()
+                    layoutManager = gridLayoutManager
+                }
+                viewModel.setAdapterData(it.recipes)
+
+            } else {
+                Toast.makeText(requireContext(), "No internet connection", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        })
+
+        return viewModel
     }
 
 }
--- app/src/main/java/com/example/recipeapplication/viewmodels/RecipesViewModel.kt
+++ app/src/main/java/com/example/recipeapplication/viewmodels/RecipesViewModel.kt
@@ -1,4 +1,53 @@
 package com.example.recipeapplication.viewmodels
 
-class RecipesViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.example.recipeapplication.data.adapters.RecipeAdapter
+import com.example.recipeapplication.data.models.Recipe
+import com.example.recipeapplication.data.models.RecipeX
+import com.example.recipeapplication.data.network.RetrofitBuilder
+import kotlinx.coroutines.launch
+import retrofit2.await
+import java.io.IOException
+
+class RecipesViewModel(application: Application) : AndroidViewModel(application) {
+    private val context = getApplication<Application>().applicationContext
+
+    /**
+     *  LiveData gives us updated data when they change.
+     * */
+    private var allRecipes: MutableLiveData<Recipe> = MutableLiveData()
+
+    private var adapter: RecipeAdapter
+
+    init {
+        fetchMurdersInCoroutine()
+        adapter = RecipeAdapter()
+    }
+
+    fun getMyAdapter(): RecipeAdapter {
+        return adapter
+    }
+
+    fun setAdapterData(data: List<RecipeX>) {
+        adapter.apply {
+            setDataList(data)
+            notifyDataSetChanged()
+        }
+    }
+
+    fun getAllRecipesData(): MutableLiveData<Recipe> {
+        return allRecipes
+    }
+
+    private fun fetchMurdersInCoroutine() = viewModelScope.launch {
+        try {
+            val fetchingMurders = RetrofitBuilder.apiService.getAllRecipes().await()
+            allRecipes.postValue(fetchingMurders)
+        } catch (networkError: IOException) {
+            allRecipes.postValue(null)
+        }
+    }
 }
